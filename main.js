246aa07ebd5268d8ccd4206e845007e80931170a
Umbeee <umberto.nesta@hotmail.com>
2023-04-04 19:35:08 +0200

    esercizio completo, da decorare

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+let arrayNumeri = []
+let arrayNumeriDue = []
+
 function numRandom(min, max){
     return Math.floor(Math.random() * max) + min
 }
@@ -9,7 +12,7 @@ btnGioca.addEventListener('click', function(){
 
 function memoryNumeri(){
 
-    let arrayNumeri = []
+    
 
     for(let i=0; i<5; i++){
         arrayNumeri.push(numRandom(1, 50))
@@ -25,6 +28,8 @@ function memoryNumeri(){
     }
 
     setTimeout(secondaParte, 3000);
+
+   
 
 }
 
@@ -47,11 +52,30 @@ function secondaParte(){
 
 function funzionePrompt(){
 
-    let arrayNumeriDue = []
-
     for(let i=0; i<5; i++){
         arrayNumeriDue.push(prompt(`Inserisci numero ${i+1}`))
     }
     console.log(arrayNumeriDue)
+
+    let contatore = 0;
+
+    for(let i=0; i<5; i++)
+    {
+        for(let j=0; j<5; j++){
+            if(arrayNumeri[i]==arrayNumeriDue[j]){
+                contatore++
+                let controllo = document.createElement('p')
+                controllo.innerText=`il numero ${arrayNumeri[i]} è giusto`
+                document.getElementById('divControllo').append(controllo)
+            }
+        }
+    }
+
+    let controllo = document.createElement('p')
+    controllo.innerText=`hai azzeccato un totale di ${contatore} numeri`
+    document.getElementById('divControllo').append(controllo)
+
+    console.log(contatore)
+
 
 }
