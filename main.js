f83c083c6eb903b3a0b80b7ed374829fc6b68945
Umbeee <umberto.nesta@hotmail.com>
2023-04-04 17:38:07 +0200

    correzione prima parte, implementato array

--- main.js
+++ main.js
@@ -2,11 +2,32 @@ function numRandom(min, max){
     return Math.floor(Math.random() * max) + min
 }
 
+let btnGioca = document.getElementById('gioca')
+btnGioca.addEventListener('click', function(){
+    memoryNumeri();
+})
+
+function memoryNumeri(){
+
+    let arrayNumeri = []
+
+    for(let i=0; i<5; i++){
+        arrayNumeri.push(numRandom(1, 50))
+    }
+    console.log(arrayNumeri)
+
+    for(let j=0; j<arrayNumeri.length; j++){
+        let spanNum = document.createElement('span');
+        spanNum.classList.add('spanNum');
+        spanNum.innerText = arrayNumeri[j];
+
+        document.querySelector('main').append(spanNum)
+    }
+
+    setTimeout(secondaParte, 300);
+}
 
 
-let numero1 = document.writeln(numRandom(1, 50))
-let numero2 = document.writeln(numRandom(1, 50))
-let numero3 = document.writeln(numRandom(1, 50))
-let numero4 = document.writeln(numRandom(1, 50))
-let numero5 = document.writeln(numRandom(1, 50))
-
+function secondaParte(){
+    
+}
