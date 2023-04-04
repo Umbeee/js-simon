155431b400147125d07043f15a41b7cbfddc500f
Umbeee <umberto.nesta@hotmail.com>
2023-04-04 17:13:04 +0200

    cinque numeri compaiono correttamente

---
+++ main.js
@@ -0,0 +1,12 @@
+function numRandom(min, max){
+    return Math.floor(Math.random() * max) + min
+}
+
+
+
+let numero1 = document.writeln(numRandom(1, 50))
+let numero2 = document.writeln(numRandom(1, 50))
+let numero3 = document.writeln(numRandom(1, 50))
+let numero4 = document.writeln(numRandom(1, 50))
+let numero5 = document.writeln(numRandom(1, 50))
+
